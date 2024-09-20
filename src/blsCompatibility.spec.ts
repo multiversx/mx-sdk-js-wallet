c0f37ec7ac30428a41d59908c920055bc189e63b
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2024-09-20 22:10:57 +0300

    setArrayMaskLikeHerumi().

--- src/blsCompatibility.spec.ts
+++ src/blsCompatibility.spec.ts
@@ -2,20 +2,68 @@ import * as nobleUtils from "@noble/curves/abstract/utils";
 import { bls12_381 as nobleBls } from "@noble/curves/bls12-381";
 import { assert } from "chai";
 
-describe("test BLS compatibility (noble crypto and herumi)", () => {
-    it("test BLS implementations", async function () {
-    });
+const Fp = nobleBls.fields.Fp;
+
+describe.only("test BLS compatibility (noble crypto and herumi)", () => {
+    it("test BLS implementations", async function () {});
 
     it("test getWeierstrassLikeHerumi", async function () {
-        const inputHex = "b14695c802ca943acc28d5e47aec2ce163d3004559fc9d2e1659f5f22ca363f96548e504a6f2b9cab57bcce75c4e9309";
-        const expectedOutputHex = "fd15a70e718737b6457701e2c134b254d797837f7166300f46360974e3b51ac4d679f7de76d488d52da1c13a3f1bb719";
+        const inputHex =
+            "b14695c802ca943acc28d5e47aec2ce163d3004559fc9d2e1659f5f22ca363f96548e504a6f2b9cab57bcce75c4e9309";
+        const expectedOutputHex =
+            "fd15a70e718737b6457701e2c134b254d797837f7166300f46360974e3b51ac4d679f7de76d488d52da1c13a3f1bb719";
         const input = nobleUtils.bytesToNumberLE(Buffer.from(inputHex, "hex"));
         const output = getWeierstrassLikeHerumi(input);
-        const outputHex = Buffer.from(nobleUtils.numberToBytesLE(output, 48)).toString("hex");
-        
+        const outputHex = Buffer.from(nobleUtils.numberToBytesLE(output, Fp.BYTES)).toString("hex");
+
+        assert.equal(outputHex, expectedOutputHex);
+    });
+
+    it("test setArrayMaskLikeHerumi", async function () {
+        const inputHex =
+            "f74f2603939a53656948480ce71f1ce466685b6654fd22c61c1f2ce4e2c96d1cd02d162b560c4beaf1ae45f3471dc5cbc1ce040701c0b5c38457988aa00fe97f";
+        const expectedOutputHex =
+            "f74f2603939a53656948480ce71f1ce466685b6654fd22c61c1f2ce4e2c96d1cd02d162b560c4beaf1ae45f3471dc50b";
+
+        const input = Buffer.from(inputHex, "hex");
+        const output = setArrayMaskLikeHerumi(input);
+        const outputHex = Buffer.from(output).toString("hex");
+
         assert.equal(outputHex, expectedOutputHex);
     });
 });
+
+// Herumi code: https://github.com/herumi/mcl/blob/v2.00/include/mcl/fp.hpp#L371
+// x &= (1 << bitLen) - 1
+// x &= (1 << (bitLen - 1)) - 1 if x >= p
+// void setArrayMask(const S *x, size_t n)
+// {
+//     const size_t dstByte = sizeof(Unit) * op_.N;
+//     if (sizeof(S) * n > dstByte) {
+//         n = dstByte / sizeof(S);
+//     }
+//     bool b = fp::convertArrayAsLE(v_, op_.N, x, n);
+//     assert(b);
+//     (void)b;
+//     bint::maskN(v_, op_.N, op_.bitSize);
+//     if (bint::cmpGeN(v_, op_.p, op_.N)) {
+//         bint::maskN(v_, op_.N, op_.bitSize - 1);
+//     }
+//     toMont();
+// }
+function setArrayMaskLikeHerumi(x: Uint8Array): Uint8Array {
+    const mask1 = nobleUtils.bitMask(Fp.BITS);
+    const mask2 = nobleUtils.bitMask(Fp.BITS - 1);
+
+    let xAsBigInt = nobleUtils.bytesToNumberLE(x);
+    xAsBigInt = xAsBigInt & mask1;
+
+    if (xAsBigInt >= Fp.ORDER) {
+        xAsBigInt = xAsBigInt & mask2;
+    }
+
+    return nobleUtils.numberToBytesLE(xAsBigInt, Fp.BYTES);
+}
 
 // Herumi code: https://github.com/herumi/mcl/blob/v2.00/include/mcl/ec.hpp#L1954
 // void getWeierstrass(Fp& yy, const Fp& x)
@@ -26,14 +74,14 @@ describe("test BLS compatibility (noble crypto and herumi)", () => {
 //     t *= x;
 //     Fp::add(yy, t, b_);
 // }
-function getWeierstrassLikeHerumi(x: any) {
-    const a = nobleBls.fields.Fp.ZERO;
+function getWeierstrassLikeHerumi(x: bigint): bigint {
+    const a = Fp.ZERO;
     const b = nobleBls.G1.CURVE.b;
-  
-    let t = nobleBls.fields.Fp.sqr(x);
-    t = nobleBls.fields.Fp.add(t, a);
-    t = nobleBls.fields.Fp.mul(t, x);
-    const yy = nobleBls.fields.Fp.add(t, b);
+
+    let t = Fp.sqr(x);
+    t = Fp.add(t, a);
+    t = Fp.mul(t, x);
+    const yy = Fp.add(t, b);
     return yy;
 }
 
@@ -42,10 +90,16 @@ function getWeierstrassLikeHerumi(x: any) {
 // const char *c1 = "be32ce5fbeed9ca374d38c0ed41eefd5bb675277cdf12d11bc2fb026c41400045c03fffffffdfffd";
 // const char *c2 = "5f19672fdf76ce51ba69c6076a0f77eaddb3a93be6f89688de17d813620a00022e01fffffffefffe";
 function getHerumiConstants() {
-    const c1Bytes = Buffer.from("fdfffdffffff035c040014c426b02fbc112df1cd775267bbd5ef1ed40e8cd374a39cedbe5fce32be0000000000000000", "hex");
-    const c2Bytes = Buffer.from("fefffeffffff012e02000a6213d817de8896f8e63ba9b3ddea770f6a07c669ba51ce76df2f67195f0000000000000000", "hex");
+    const c1Bytes = Buffer.from(
+        "fdfffdffffff035c040014c426b02fbc112df1cd775267bbd5ef1ed40e8cd374a39cedbe5fce32be0000000000000000",
+        "hex",
+    );
+    const c2Bytes = Buffer.from(
+        "fefffeffffff012e02000a6213d817de8896f8e63ba9b3ddea770f6a07c669ba51ce76df2f67195f0000000000000000",
+        "hex",
+    );
     const c1 = nobleUtils.bytesToNumberLE(c1Bytes);
     const c2 = nobleUtils.bytesToNumberLE(c2Bytes);
-    
+
     return { c1, c2 };
 }
